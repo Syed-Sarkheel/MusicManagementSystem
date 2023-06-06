32ddf9945ac985839530e3b637122fbe65b9dc7c
Syed Sarkheel <81019807+Syed-Sarkheel@users.noreply.github.com>
2023-06-06 20:16:55 +0530

    Update Song.java

--- src/main/java/org/example/Song.java
+++ src/main/java/org/example/Song.java
@@ -1,2 +1,38 @@
-package org.example;public class j {
+package org.example;
+
+import java.io.*;
+import java.util.*;
+
+// Class to represent a song
+class Song {
+    private String title;
+    private String artist;
+    private int duration;
+
+    public Song(String title, String artist, int duration) {
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Artist: " + artist + ", Duration: " + duration + " seconds";
+    }
 }
+
+// Class to manage the music library
+
+// Main class
